cb1962e9763230b66f5513dfece88dde38426d35
Shicheng Chu <cheng61746093@gmail.com>
2018-01-09 16:01:57 -0500

    Secnond day
    Secnond day

--- generator.py
+++ generator.py
@@ -3,9 +3,15 @@ import Utility as u
 
 u.test()
 
-file = open("mitsuba.xml", "w+")
+file = open("C:\\Users\\chu.386\\Desktop\\FinalScene\\mitsuba.xml", "w+")
 
 u.print_head_Info(file)
 u.print_scene_head(file)
 u.integrator(file)
+u.perspective(file)
+u.bsdf_water_drop(file)
+u.envmap(file)
+u.glass(file)
+u.test_obj(file)
+
 u.print_scene_tail(file)
