f2d6ef291aed37c28cf405adc732375e07233a98
Shicheng Chu <cheng61746093@gmail.com>
2018-01-10 18:13:36 -0500

    Use python to open mitsuba
    Use python to open mitsuba, still have bugs:
    Caught a critical exception: main [scenehandler.cpp:819] Fatal error in file "" (line 0): unable to open primary document entity

--- generator.py
+++ generator.py
@@ -1,8 +1,10 @@
-import os
+import os, sys
+
+import subprocess
+
 import Utility as u
 
-u.test()
-
+#C:\\Users\\chu.386\\Desktop\\FinalScene\\
 file = open("C:\\Users\\chu.386\\Desktop\\FinalScene\\mitsuba.xml", "w+")
 
 u.print_head_Info(file)
@@ -12,6 +14,24 @@ u.perspective(file)
 u.bsdf_water_drop(file)
 u.envmap(file)
 u.glass(file)
+
 u.test_obj(file)
 
+
 u.print_scene_tail(file)
+
+
+subprocess.check_output("C:\\Users\\chu.386\\Desktop\\Mitsuba0.5.0\\mitsuba.exe C:/Users/chu.386/Desktop/FinalScene/mitsuba.xml", shell=True)
+#cmd = ["cd", "C:\\Users\\chu.386\\Desktop\\FinalScene\\"]
+#subprocess.call(cmd)
+#cmd_out = subprocess.check_output(cmd)
+
+#print(cmd_out)
+
+#cmd = ["mitsuba", "mitsuba.xml"]
+#subprocess.call(cmd)
+
+#cmd_out = subprocess.check_output(cmd)
+
+#print(cmd_out)
+
