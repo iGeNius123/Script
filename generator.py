1f4a1e48d41d6ab2e393adb074ae6af69c9b7ee0
Shicheng Chu <cheng61746093@gmail.com>
2018-01-13 17:03:38 -0500

    Alpha version
    Basic function completed

--- generator.py
+++ generator.py
@@ -4,24 +4,46 @@ import subprocess
 
 import Utility as u
 
+
+def no_split_long_drops(n,f):
+    f.write("	<shape type=\"obj\">\n")
+    str ="		<string name=\"filename\" value=\"No_Split_Long_Drops/dynamic_drop_"+'{num:03d}'.format(num=n)+".obj\"/>\n"
+    f.write(str)
+    f.write("			<transform name=\"toWorld\">\n")
+    f.write("		    <scale x=\"0.2\" y=\"0.2\" z=\"0.5\"/>\n")
+    f.write("		</transform>\n")
+    f.write("		<ref id=\"Water_Drop\"/>\n")
+    f.write("	</shape>\n")
+
+
 #C:\\Users\\chu.386\\Desktop\\FinalScene\\
-file = open("C:\\Users\\chu.386\\Desktop\\FinalScene\\mitsuba.xml", "w+")
-
-u.print_head_Info(file)
-u.print_scene_head(file)
-u.integrator(file)
-u.perspective(file)
-u.bsdf_water_drop(file)
-u.envmap(file)
-u.glass(file)
-
-u.test_obj(file)
 
 
-u.print_scene_tail(file)
+for i in range(1, 271):
+    file = open("C:\\Users\\chu.386\\Desktop\\FinalScene\\mitsuba.xml", "w+")
+    u.print_head_Info(file)
+    u.print_scene_head(file)
+    u.integrator(file)
+    u.perspective(file)
+    u.bsdf_water_drop(file)
+    u.envmap(file)
+    u.glass(file)
+    no_split_long_drops(i,file)
+    u.print_scene_tail(file)
 
 
-subprocess.check_output("C:\\Users\\chu.386\\Desktop\\Mitsuba0.5.0\\mitsuba.exe C:/Users/chu.386/Desktop/FinalScene/mitsuba.xml", shell=True)
+
+    file.close()
+
+
+    name = '{num:03d}'.format(num=i)
+    cmd="cd C:\\Users\\chu.386\\Desktop\\FinalScene && mitsuba -o "+name+" mitsuba.xml"
+    os.system(cmd)
+    move = "move  C:\\Users\\chu.386\\Desktop\\FinalScene\\"+name+".png  C:\\Users\\chu.386\\Desktop\\FinalScene\\Result"
+    os.system(move)
+
+#os.system("start \"\" cmd /k \"cd /D C:\\ ")
+#subprocess.check_output("C:\\Users\\chu.386\\Desktop\\Mitsuba0.5.0\\mitsuba.exe C:/Users/chu.386/Desktop/FinalScene/mitsuba.xml", shell=True)
 #cmd = ["cd", "C:\\Users\\chu.386\\Desktop\\FinalScene\\"]
 #subprocess.call(cmd)
 #cmd_out = subprocess.check_output(cmd)
